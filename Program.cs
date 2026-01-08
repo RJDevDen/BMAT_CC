12814d7faab949d0c257c57836fdeb88db2d60fc
RJ <rjdevden@gmail.com>
2026-01-08 00:11:01 +0000

    Attempting to fix exe home folder error

--- Program.cs
+++ Program.cs
@@ -9,19 +9,37 @@ namespace BMAT_CC_Host
     {
         static void Main(string[] args)
         {
-            var assembly = Assembly.GetExecutingAssembly();
-            // Note: Replace 'BMAT_CC' with your actual Project Name if different
-            var resourceName = "BMAT_CC.BMAT_CC.ps1";
-
-            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-            using (StreamReader reader = new StreamReader(stream))
+            try
             {
-                string scriptContents = reader.ReadToEnd();
-                using (PowerShell ps = PowerShell.Create())
+                // This fix tells PowerShell that "Home" is wherever this EXE is running
+                string appPath = AppContext.BaseDirectory;
+                Environment.SetEnvironmentVariable("PSHOME", appPath);
+
+                var assembly = Assembly.GetExecutingAssembly();
+                // Replace 'BMAT_CC' with your actual Namespace/Project name if needed
+                var resourceName = "BMAT_CC.BMAT_CC.ps1";
+
+                using (Stream stream = assembly.GetManifestResourceStream(resourceName))
                 {
-                    ps.AddScript(scriptContents);
-                    ps.Invoke();
+                    if (stream == null) throw new Exception($"Could not find embedded script: {resourceName}");
+
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        string scriptContents = reader.ReadToEnd();
+                        using (PowerShell ps = PowerShell.Create())
+                        {
+                            ps.AddScript(scriptContents);
+                            ps.Invoke();
+                        }
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("FATAL ERROR: " + ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
             }
         }
     }
